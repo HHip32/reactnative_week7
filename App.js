570f420554b8e2fb5c75a500ea4af99ab6036c75
hhiep <hoaihiep12b1thptlochung2020@gmail.com>
2023-10-20 18:14:56 +0700

    Cap nhat trang thu nhat, them bottomTabNavigationBar

--- App.js
+++ App.js
@@ -1,164 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable, FlatList } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import index1 from './index1';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-export default function App() {
+
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <View style={{ width: '100%', height: '42px', backgroundColor: '#1BA9FF', alignItems: 'center' }}>
-        <View style={{ width: '90%', height: '42px', marginTop: 10, flexDirection: 'row', justifyContent: 'space-between' }}>
-          <Image
-            source={require('./assets/ant-design_arrow-left-outlined.png')}
-            style={{ width: '24px', height: '24px' }}
-          />
-          <Text style={{ color: '#FFFFFF', fontSize: 18, lineHeight: 23.44 }}>
-            Chat
-          </Text>
-          <Image
-            source={require('./assets/bi_cart-check.png')}
-            style={{ width: '24px', height: '24px' }}
-          />
-        </View>
-      </View>
-      <View style={{ marginTop: 10 }}>
-        <Text style={{ color: '#000000', fontSize: 12, lineHeight: 17.03, width: '294px', height: '28.54px' }}>
-          Bạn có thắc mắc với sản phẩm vừa xem. Đừng ngại chát với shop!
-        </Text>
-      </View>
-      <View style={{ width: '100%', height: '1px', backgroundColor: '#C4C4C4', marginTop: '20px' }} />
-      <View style={{ width: '100%' }}>
-        <View style={{ width: '100%', height: '80px', marginTop: '5px', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', backgroundColor: '#FFFFFF' }}>
-          <Image
-            source={require('./assets/ca_nau_lau.png')}
-            style={{ width: '74px', height: '74px' }}
-          />
-          <View style={{ width: 180, right: 10, bottom: 15 }}>
-            <Text>Cá nấu lẩu, nấu mì mini...</Text>
-            <Text style={{ marginTop: 10, color: '#E83030' }}> Shop Devang</Text>
-          </View>
-          <Pressable style={{ width: '88px', height: '38px', right: 25, alignItems: 'center', justifyContent: 'center', backgroundColor: '#F31111' }}>
-            <Text style={{ fontSize: 14, color: '#FFFFFF' }}>Chat</Text>
-          </Pressable>
-        </View>
-      </View>
-      <View style={{ width: '100%', height: '1px', backgroundColor: '#C4C4C4', marginTop: '0px' }} />
-      <View style={{ width: '100%' }}>
-        <View style={{ width: '100%', height: '80px', marginTop: '1px', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', backgroundColor: '#FFFFFF' }}>
-          <Image
-            source={require('./assets/ga_bo_toi.png')}
-            style={{ width: '74px', height: '74px' }}
-          />
-          <View style={{ width: 180, right: 10, bottom: 15 }}>
-            <Text>1KG KHô GÀ BƠ TỎI ...</Text>
-            <View style={{ marginTop: 10, color: '#E83030', flexDirection:'row' }}>
-              <Text style={{color: '#7D5B5B'}}>Shop</Text>
-              <Text style={{color: '#FF0E0E', marginLeft: '10px'}}>LTD Food</Text>
-            </View>
-          </View>
-          <Pressable style={{ width: '88px', height: '38px', right: 25, alignItems: 'center', justifyContent: 'center', backgroundColor: '#F31111' }}>
-            <Text style={{ fontSize: 14, color: '#FFFFFF' }}>Chat</Text>
-          </Pressable>
-        </View>
-      </View>
-      <View style={{ width: '100%', height: '1px', backgroundColor: '#C4C4C4', marginTop: '0px' }} />
-      <View style={{ width: '100%' }}>
-        <View style={{ width: '100%', height: '80px', marginTop: '1px', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', backgroundColor: '#FFFFFF' }}>
-          <Image
-            source={require('./assets/xa_can_cau.png')}
-            style={{ width: '74px', height: '74px' }}
-          />
-          <View style={{ width: 180, right: 10, bottom: 15 }}>
-            <Text>Xe cần cẩu đa năng</Text>
-            <View style={{ marginTop: 10, color: '#E83030', flexDirection:'row' }}>
-              <Text style={{color: '#7D5B5B'}}>Shop</Text>
-              <Text style={{color: '#000000', marginLeft: '10px'}}>Thế giới đồ chơi</Text>
-            </View>
-          </View>
-          <Pressable style={{ width: '88px', height: '38px', right: 25, alignItems: 'center', justifyContent: 'center', backgroundColor: '#F31111' }}>
-            <Text style={{ fontSize: 14, color: '#FFFFFF' }}>Chat</Text>
-          </Pressable>
-        </View>
-      </View>
-      <View style={{ width: '100%', height: '1px', backgroundColor: '#C4C4C4', marginTop: '0px' }} />
-      <View style={{ width: '100%' }}>
-        <View style={{ width: '100%', height: '80px', marginTop: '1px', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', backgroundColor: '#FFFFFF' }}>
-          <Image
-            source={require('./assets/do_choi_dang_mo_hinh.png')}
-            style={{ width: '74px', height: '74px' }}
-          />
-          <View style={{ width: 180, right: 10, bottom: 15 }}>
-            <Text>Đồ chơi dạng mô hình</Text>
-            <View style={{ marginTop: 10, color: '#E83030', flexDirection:'row' }}>
-              <Text style={{color: '#7D5B5B'}}>Shop</Text>
-              <Text style={{color: '#000000', marginLeft: '10px'}}>Thế giới đồ chơi</Text>
-            </View>
-          </View>
-          <Pressable style={{ width: '88px', height: '38px', right: 25, alignItems: 'center', justifyContent: 'center', backgroundColor: '#F31111' }}>
-            <Text style={{ fontSize: 14, color: '#FFFFFF' }}>Chat</Text>
-          </Pressable>
-        </View>
-      </View>
-      <View style={{ width: '100%', height: '1px', backgroundColor: '#C4C4C4', marginTop: '0px' }} />
-      <View style={{ width: '100%' }}>
-        <View style={{ width: '100%', height: '80px', marginTop: '1px', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', backgroundColor: '#FFFFFF' }}>
-          <Image
-            source={require('./assets/lanh_dao_gian_don.png')}
-            style={{ width: '74px', height: '74px' }}
-          />
-          <View style={{ width: 180, right: 10, bottom: 15 }}>
-            <Text>Lãnh đạo giản đơn</Text>
-            <View style={{ marginTop: 10, color: '#E83030', flexDirection:'row' }}>
-              <Text style={{color: '#7D5B5B'}}>Shop</Text>
-              <Text style={{color: '#000000', marginLeft: '10px'}}>Minh Long Book</Text>
-            </View>
-          </View>
-          <Pressable style={{ width: '88px', height: '38px', right: 25, alignItems: 'center', justifyContent: 'center', backgroundColor: '#F31111' }}>
-            <Text style={{ fontSize: 14, color: '#FFFFFF' }}>Chat</Text>
-          </Pressable>
-        </View>
-      </View>
-      <View style={{ width: '100%', height: '1px', backgroundColor: '#C4C4C4', marginTop: '0px' }} />
-      <View style={{ width: '100%' }}>
-        <View style={{ width: '100%', height: '80px', marginTop: '1px', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', backgroundColor: '#FFFFFF' }}>
-          <Image
-            source={require('./assets/hieu_long_con_tre.png')}
-            style={{ width: '74px', height: '74px' }}
-          />
-          <View style={{ width: 180, right: 10, bottom: 15 }}>
-            <Text>Hiểu lòng con trẻ</Text>
-            <View style={{ marginTop: 10, color: '#E83030', flexDirection:'row' }}>
-              <Text style={{color: '#7D5B5B'}}>Shop</Text>
-              <Text style={{color: '#000000', marginLeft: '10px'}}>Minh Long Book</Text>
-            </View>
-          </View>
-          <Pressable style={{ width: '88px', height: '38px', right: 25, alignItems: 'center', justifyContent: 'center', backgroundColor: '#F31111' }}>
-            <Text style={{ fontSize: 14, color: '#FFFFFF' }}>Chat</Text>
-          </Pressable>
-        </View>
-      </View>
-      <View style={{width: '100%', height: '49px', backgroundColor: '#1BA9FF', alignItems: 'center'}}>
-        <View  style={{width: '90%', height: '49px',flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-          <Image 
-          source={require('./assets/group10.png')}
-          style={{width: '23px', height: '14px'}}
-          />
-          <Image 
-          source={require('./assets/vector.png')}
-          style={{width: '24px', height: '24px'}}
-          />
-          <Image 
-          source={require('./assets/vector1.png')}
-          style={{width: '27px', height: '25px'}}
-          />
-        </View>
-      </View>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    backgroundColor: '#E5E5E5'
-  },
-});
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+function GoBackScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Go back!</Text>
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName='Index1'
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Index1') {
+              iconName = focused
+                ? 'home'
+                : 'home-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'reorder-three' : 'reorder-three-outline';
+            }
+            else if (route.name === 'Goback') {
+              iconName = focused ? 'arrow-back' : 'arrow-back-outline';
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          headerShown: false,
+          tabBarLabel: ''
+        })}
+      >
+        <Tab.Screen name='Settings' component={SettingsScreen} />
+        <Tab.Screen name="Index1" component={index1} />  
+        <Tab.Screen name='Goback' component={GoBackScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ index1.js
@@ -0,0 +1,111 @@
+
+import { StyleSheet, Text, View, Image, Pressable, FlatList } from 'react-native';
+
+
+
+const data = [
+  {
+    id: 1,
+    path: require('./assets/ca_nau_lau.png'),
+    shopName: "Devang",
+    desciptions: "Cá nấu lẩu, nấu mì mini..."
+  },
+  {
+    id: 2,
+    path: require('./assets/ga_bo_toi.png'),
+    shopName: "LTD Food",
+    desciptions: "1KG KHÔ GÀ BƠ TỎI ..."
+  },
+  {
+    id: 3,
+    path: require('./assets/xa_can_cau.png'),
+    shopName: "Thế giới đồ chơi",
+    desciptions: "Xe cần cẩu đa năng"
+  },
+  {
+    id: 4,
+    path: require('./assets/do_choi_dang_mo_hinh.png'),
+    shopName: "Thế giới đồ chơi",
+    desciptions: "Đồ chơi dạng mô hình"
+  },
+  {
+    id: 5,
+    path: require('./assets/lanh_dao_gian_don.png'),
+    shopName: "Minh Long Book",
+    desciptions: "Lãnh đạo giản đơn"
+  },
+  {
+    id: 6,
+    path: require('./assets/hieu_long_con_tre.png'),
+    shopName: "Minh Long Book",
+    desciptions: "Hiểu lòng con trẻ"
+  },
+  {
+    id: 7,
+    path: require('./assets/trump1.png'),
+    shopName: "Minh Long Book",
+    desciptions: "Denel Trump Thiên tài lãnh đạo"
+  },
+]
+const renderItem = ({ item }) => (
+  <View style={{ width: '380px'}}>
+    <View style={{ width: '380px', height: '80px', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', backgroundColor: item.id === 1 ? '#FFFFFF' : '#E5E5E5', marginTop: item.id === 1 ? '5px' : '0px' }}>
+      <Image
+        source={item.path}
+        style={{ width: '74px', height: '74px' }}
+      />
+      <View style={{ width: 180, right: 10, bottom: 10 }}>
+        <Text style={{ marginTop: item.id === 7 ? '10px' : '0px' }}>{item.desciptions}</Text>
+        <View style={{ marginTop: 10, color: '#E83030', flexDirection: 'row' }}>
+          <Text style={{ color: item.id === 1 ? '#E83030' : '#7D5B5B' }}>Shop</Text>
+          <Text style={{ color: (item.id === 1 || item.id === 2) ? '#FF0E0E' : '#000000', marginLeft: '10px' }}>{item.shopName}</Text>
+        </View>
+      </View>
+      <Pressable style={{ width: '88px', height: '38px', right: 25, alignItems: 'center', justifyContent: 'center', backgroundColor: '#F31111' }}>
+        <Text style={{ fontSize: 14, color: '#FFFFFF' }}>Chat</Text>
+      </Pressable>
+    </View>
+    <View style={{ width: '100%', height: '1px', backgroundColor: '#C4C4C4', marginTop: '0px' }} />
+  </View>
+);
+
+export default function index1() {
+  return (
+    <View style={styles.container}>
+      <View style={{ width: '100%', height: '42px', backgroundColor: '#1BA9FF', alignItems: 'center' }}>
+        <View style={{ width: '90%', height: '42px', marginTop: 10, flexDirection: 'row', justifyContent: 'space-between' }}>
+          <Image
+            source={require('./assets/ant-design_arrow-left-outlined.png')}
+            style={{ width: '24px', height: '24px' }}
+          />
+          <Text style={{ color: '#FFFFFF', fontSize: 18, lineHeight: 23.44 }}>
+            Chat
+          </Text>
+          <Image
+            source={require('./assets/bi_cart-check.png')}
+            style={{ width: '24px', height: '24px' }}
+          />
+        </View>
+      </View>
+      <View style={{ marginTop: 10 }}>
+        <Text style={{ color: '#000000', fontSize: 12, lineHeight: 17.03, width: '294px', height: '28.54px' }}>
+          Bạn có thắc mắc với sản phẩm vừa xem. Đừng ngại chát với shop!
+        </Text>
+      </View>
+      <View style={{ width: '100%', height: '1px', backgroundColor: '#C4C4C4', marginTop: '20px' }} />
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={item => item.id.toString()}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#E5E5E5'
+  },
+});
